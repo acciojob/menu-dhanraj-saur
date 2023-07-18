0b703483e59429778f64862a11f5de02bc235c20
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-18 23:51:58 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import './../styles/App.css';
 import DishList from './DishList';
-import CategoryFilter from './CategoryFilter';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Delicious Dishes</h1>
-            <CategoryFilter />
-            <DishList />
-        </div>
-    );
+  return (
+    <div>
+        
+      <h1>Delicious Dishes</h1>
+      <DishList />
+    </div>
+  );
 };
-
 export default App;
